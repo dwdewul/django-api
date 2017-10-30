9a4768f48b572b66c0989a5c698ca1907afb31b3
David DeWulf <dwdewul@gmail.com>
2017-10-30 10:28:47 -0500

    Added serializer and HTTP method handlers

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import HelloSerializer
+from rest_framework import status
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,4 +18,32 @@ class HelloAPIView(APIView):
             'It is similar to a traditional django view'
         ]
 
-        return Response({ 'message': 'Hello!', 'an_apiview': an_apiview })
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Updates an object"""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Only update fields provided in request"""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Delete a specified object"""
+
+        return Response({'method': 'delete'})
